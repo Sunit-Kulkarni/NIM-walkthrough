1f638d51ee16f662c8cd5f7b86c69c1f35db85d8
Sunit Kulkarni <sunitkulkarni@Sunits-MacBook-Pro-2.local>
2017-09-12 18:56:23 -0400

    commenting the structure of the impending code - providing a roadmap for development

---
+++ javascript.js
@@ -0,0 +1,25 @@
+//NIM - THE GAME
+
+//DEFINE THE STATE VARIABLES
+// - Number of Dots
+// - Who's turn it is
+// - How many players
+// - How many dots to pick up
+
+
+//ACTIONS (these are functions that mess with the state)
+// - Print a bunch of dots
+// - remove dots
+// - change who's turn it is
+// - print the board
+// - check for a winner
+
+
+//KICKING EVERYTHING OFF
+// - Show the board right away
+// - Prompt the user for how manuy dots the want to remove
+// - Actually remove the dots
+// - Print the new result
+// - Switch players
+// - Check to see if they won
+// - Keep looping ^^^ until someone wins
